30d97a7bcad771246f98b6cf119da5ad7d701412
m.hassan@sprints.ws <sprintsws_123123>
2020-02-20 19:56:52 +0200

    Test git

--- app/src/main/java/com/task/task/MainActivity.kt
+++ app/src/main/java/com/task/task/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var test = "test"
     }
 
 }
